49cfa719e3be550095e2557a9e436e1dc48b51df
Kuba Kołodziej <150014607+jkolodziej-bergregions@users.noreply.github.com>
2024-03-13 21:28:56 +0100

    add basic footer without styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React from 'react';
+import './Footer.scss'; // Assuming you'll use SCSS for styling
+import { FaFacebook, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
-  <>
-    <p>This is footer which would be beautiful</p>
-  </>
-}
+  return (
+    <footer className="site-footer">
+      <div className="container">
+          <p>Contact us: madet@gmail.com</p>
+          <p>Follow us on social media</p>
+        <div className='social-icons'>
+          <a href='/fb'><FaFacebook /></a>
+          <a href='/fb'><FaInstagram /></a>
+          <a href='/fb'><FaYoutube /></a>
+        </div>
+        <div className="footer-bottom">
+          <p>&copy; {new Date().getFullYear()} Madet. All rights reserved.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -21,9 +21,13 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeNavbar = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className={`navbar ${isOpen ? 'open' : ''}`}>
-      <NavLink to="/" className="logo-link"> 
+      <NavLink to="/" className="logo-link" onClick={closeNavbar}> 
         <img 
           src="https://www.creativefabrica.com/wp-content/uploads/2021/03/20/Mountain-logo-Design-Graphics-9785421-1-1-580x435.png" 
           alt="LOGO" 
@@ -35,16 +39,16 @@ const Navbar = () => {
         <span></span>
       </button>
       <div className={`nav-links ${isOpen ? 'open' : ''}`}>
-        <NavLink to="/" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
+        <NavLink to="/"  onClick={closeNavbar} className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
           Home
         </NavLink>
-        <NavLink to="/about" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
+        <NavLink to="/about" onClick={closeNavbar} className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
           About
         </NavLink>
-        <NavLink to="/contact" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
+        <NavLink to="/contact" onClick={closeNavbar} className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
           Contact
         </NavLink>
-        <NavLink to="/products" className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
+        <NavLink to="/products" onClick={closeNavbar} className={({ isActive }) => (isActive ? 'nav-link active' : 'nav-link')}>
           Products
         </NavLink>
       </div>

583c7f519f55731bd6e59fc3f388590025755e2d
Kuba Kołodziej <150014607+jkolodziej-bergregions@users.noreply.github.com>
2024-03-22 09:27:26 +0100

    change article frontend view

--- src/components/AddArticle/AddArticle.tsx
+++ src/components/AddArticle/AddArticle.tsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
 import { useAppDispatch } from '../../redux/hooks/useAppDispatch';
 import { addArticle } from '../../redux/actions/ArticlesActions';
+import { useTranslation } from 'react-i18next';
 
 
 const AddArticle: React.FC = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [image, setImage] = useState('');
+
   const dispatch = useAppDispatch();
+  const { t } = useTranslation('translations');
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    dispatch(addArticle({ title, content }))
+    dispatch(addArticle({ title, content, image }))
       .then(() => {
         setTitle('');
         setContent('');
+        setImage('');
       })
       .catch((error) => {
         console.error('Wystąpił błąd:', error);
@@ -23,21 +28,28 @@ const AddArticle: React.FC = () => {
   return (
     <div className="max-w-md mx-auto mt-10">
       <form onSubmit={handleSubmit} className="flex flex-col">
+        <input 
+          type='text'
+          placeholder='image-src'
+          value={image}
+          onChange={e => setImage(e.target.value)}
+          className='mb-4 p-2 border rounded'
+        />
         <input
           type="text"
-          placeholder="Tytuł artykułu"
+          placeholder={t('articles.title_placeholder')}
           value={title}
           onChange={e => setTitle(e.target.value)}
           className="mb-4 p-2 border rounded"
         />
         <textarea
-          placeholder="Treść artykułu"
+          placeholder={t('articles.content_placeholder')}
           value={content}
           onChange={e => setContent(e.target.value)}
           className="mb-4 p-2 border rounded h-40"
         />
         <button type="submit" className="bg-blue-500 text-white p-2 rounded">
-          Dodaj artykuł
+          {t('articles.add_article_button')}
         </button>
       </form>
     </div>
--- src/pages/Articles/ArticleList/ArticleList.tsx
+++ src/pages/Articles/ArticleList/ArticleList.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ArticleList.scss';
 import { Link } from 'react-router-dom';
 import { deleteArticle, fetchArticles } from '../../../redux/actions/ArticlesActions';
 import { useAppDispatch } from '../../../redux/hooks/useAppDispatch';
 import { useAppSelector } from '../../../redux/hooks/useAppSelector';
+import { useTranslation } from 'react-i18next';
+import { HiOutlineDotsHorizontal } from "react-icons/hi";
 
 interface ArticlesListProps {
   limit?: number;
@@ -13,11 +15,15 @@ interface Article {
   id: string;
   title: string;
   content: string;
+  image: string;
 }
 
 const ArticlesList: React.FC<ArticlesListProps> = ({ limit }) => {
   const articles = useAppSelector(state => state.articles.articles);
   const dispatch = useAppDispatch();
+  const { t } = useTranslation('translations');
+
+  const [user, setUser] = useState(false);
 
   useEffect(() => {
     dispatch(fetchArticles());
@@ -32,16 +38,34 @@ const ArticlesList: React.FC<ArticlesListProps> = ({ limit }) => {
 
   return (
     <div className="my-8 mx-4">
-      <h2 className="text-3xl font-semibold text-center text-gray-800 mb-6">Nasze artykuły</h2>
+      <HiOutlineDotsHorizontal className="article-dot-icon" size={60} />
+      <h2 className="text-3xl font-semibold text-center mb-6">
+        {t('articles.header')}
+      </h2>
       <div className="articlesList flex flex-wrap justify-center gap-4">
         {articles.slice(0, limit || articles.length).map((article : Article) => (
           <article key={article.id} className="article bg-white rounded-lg shadow-md hover:shadow-lg transition-transform duration-300 ease-in-out">
-            <h3 className="title text-xl font-semibold m-4">{article.title}</h3>
+            <img src={article.image} alt="img" />
+            <h3 className="title text-xl font-semibold m-4">
+              {article.title}
+            </h3>
             <p className="content text-gray-600 flex-grow m-4">
               {article.content ? article.content.substring(0, 100) : 'Brak treści'}...
             </p>
-            <Link to={`/article/${article.id}`} className="block text-center w-full py-2 bg-blue-500 text-white rounded-b-lg hover:bg-blue-700 transition-colors duration-300 read-more">Czytaj więcej</Link>
-            <button onClick={() => handleDelete(article.id)} className="block text-center w-full py-2 bg-blue-500 text-white rounded-b-lg hover:bg-blue-700 transition-colors duration-300 delete-button">Usuń artykuł</button>
+            <Link 
+              to={`/article/${article.id}`} 
+              className="py-2 mb-4 rounded read-more"
+            >
+              {t('articles.read_more_button')}
+            </Link>
+            {user && (
+              <button 
+                onClick={() => handleDelete(article.id)} 
+                className="py-2 mt-2 mb-2 rounded delete-button"
+              >
+                {t('articles.delete_article_button')}
+              </button>
+            )}
           </article>
         ))}
       </div>
